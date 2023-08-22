df6f1b8c802ddcd0163f2fcf92b63ad9c342f045
qpq11 <142691734+qpq11@users.noreply.github.com>
2023-08-22 19:19:59 +0300

    Add files via upload

--- quad.cpp
+++ quad.cpp
@@ -2,42 +2,50 @@
 #include "TXLib.h"
 #include <math.h>
 #define EPS 1e-15
+void Input(double* a, double* b, double* c);
+
+enum RootsCount
+{
+	InfiniteRoots = -1,
+	NoRoots,
+	OneRoot, 
+	TwoRoots
+};
+
+enum EquationType{
+	LinearEq,
+	SquareEq
+};
+
 int SqSolve(double a, double b, double c, 
 			double* x1, double* x2);
-int rootCnt(double a, double b, double c, 
+EquationType TypeEquation(double a, double b, double c, 
 			double* x1, double* x2);
 int LnSolve(double b, double c, double* x1);			
 int isNan(double n);
 void clearBuffer(void);
 int main(void)
 {	
-	double a=0.0, b=0.0, c=0.0;
+	double a = 0.0, b = 0.0, c = 0.0;
 	printf("Введите коэффициенты a, b и c уравнения типа a*x^2+b*x+c=0\n");
-	while(scanf("%lf %lf %lf", &a, &b, &c)!=3){
-			printf("Некорректный ввод!\n");
-			clearBuffer();
-		}
-	clearBuffer();
-	printf("%lf, %lf, %lf",a,b,c);
+	Input(&a,&b,&c);
+	RootsCount rcount;
 	double x1=0.0, x2=0.0;
-	int rt;
-	rt=rootCnt(a,b,c,&x1,&x2);
-	int am;
-	if (rt)
-		am=SqSolve(a,b,c,&x1,&x2);
+	if (TypeEquation(a, b, c, &x1, &x2) == SquareEq)
+		rcount=(RootsCount) SqSolve(a, b, c, &x1, &x2);
 	else
-		am=LnSolve(b,c,&x1);
-	switch(am){
-		case -1: 
+		rcount=(RootsCount) LnSolve(b, c, &x1);
+	switch(rcount){
+		case InfiniteRoots:  
 			puts("Бесконечно решений");
 			break;
-		case 0:
+		case NoRoots:
 			puts("Решений нет");
 			break;
-		case 1:
+		case OneRoot:
 			printf("Решение одно, это %lf", x1);
 			break;
-		case 2:
+		case TwoRoots:
 			printf("Решений два, это %lf и %lf", x1, x2);
 			break;
 		default:
@@ -47,12 +55,12 @@ int main(void)
 	return 0;
 }
 
-int rootCnt(double a, double b, double c, 
+EquationType TypeEquation(double a, double b, double c, 
 			double* x1, double* x2)
 {	
-	assert(x1!=NULL);
-	assert(x2!=NULL);
-	assert(x1!=x2);
+	assert(x1 != NULL);
+	assert(x2 != NULL);
+	assert(x1 != x2);
 	assert(isNan(a));
 	assert(isNan(b));
 	assert(isNan(c));
@@ -60,18 +68,30 @@ int rootCnt(double a, double b, double c,
 	assert(isfinite(b));
 	assert(isfinite(c));
 	int status = 1;
-	if (fabs(a)<fabs(EPS))
+	if (fabs(a) < fabs(EPS))
 		{	
-			status=0;
+			status = 0;
 		}
-	return status;
+	return (EquationType) status;
+}
+
+void Input(double* a, double* b, double* c)
+{
+	printf("Введите коэффициенты a, b и c уравнения типа a*x^2+b*x+c=0\n");
+	while(scanf("%lf %lf %lf", a, b, c) != 3)
+		{
+			printf("Некорректный ввод!\n");
+			clearBuffer();
+		}
+	clearBuffer();
+	printf("%lf, %lf, %lf", *a, *b, *c);
 }
 int LnSolve(double b, double c, double* x1)
 	{
 		printf("\nУравнение не квадратное\n");
-		if ((fabs(b)<fabs(EPS))&&(fabs(c)<fabs(EPS)))
+		if ( (fabs(b) < fabs(EPS)) && (fabs(c) < fabs(EPS)) )
 			return -1;
-		else if (fabs(b)<fabs(EPS))
+		else if (fabs(b) < fabs(EPS))
 			return 0;
 		else
 			*x1=-c/b;
@@ -81,28 +101,28 @@ int LnSolve(double b, double c, double* x1)
 int SqSolve(double a, double b, double c, 
 			double* x1, double* x2)
 	{		
-			const double d=(b*b)-(4.0*a*c);
-			printf("\nДискриминант уравнения равен %lf\n",d);
-			if (d<-EPS)
-			{
-				return 0;
-			}
-			else if (fabs(d)<fabs(EPS))
-			{
-				*x1=-b/(2.0*a);
-				return 1;
-			}
-			else
-			{
-				*x1=(-b+sqrt(d))/(2.0*a); 
-				*x2=(-b-sqrt(d))/(2.0*a);
-				return 2;
-			}
+		const double d = (b * b) - (4.0 * a * c);
+		printf("\nДискриминант уравнения равен %lf\n",d);
+		if (d < -EPS)
+		{
+			return 0;
+		}
+		else if (fabs(d) < fabs(EPS))
+		{
+			*x1 = -b / (2.0 * a);
+			return 1;
+		}
+		else
+		{
+			*x1 = (-b + sqrt(d)) / (2.0 * a); 
+			*x2 = (-b - sqrt(d)) / (2.0 * a);
+			return 2;
+		}
 	}
 
 int isNan(double n)
 {
-	if (n==n)
+	if (n == n)
 		return 1;
 	else
 		return 0;
@@ -110,7 +130,7 @@ int isNan(double n)
 
 void clearBuffer(void){
 	int ch;
-	while(((ch=getchar())!='\n')&&(ch!=EOF)){
+	while(((ch=getchar()) != '\n') && (ch != EOF)){
 		continue;
 	}
 }
